f8083586dd701f582295ba4d63197675a85d67cb
stefani-nik <stefani_nik@abv.bg>
2018-03-02 23:45:16 +0200

    Fixing app architecture

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import Header from './Header';
+import Header from './components/Header';
 
-import './App.css';
+import './styles/App.css';
 
 class App extends Component {
     render() {
